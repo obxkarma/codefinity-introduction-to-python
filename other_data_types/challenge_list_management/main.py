c6ec7281ece7057fc244cc5c9b2744a97f66c1ee
obxkarma <>
2026-05-15 16:28:53 +0000

    Challenge: List Management

---
+++ other_data_types/challenge_list_management/main.py
@@ -0,0 +1,17 @@
+# initialing lists by product, price, qty, and type:
+meat = ["Ham", 3.99, 50, "Sliced"]
+cheese = ["Cheddar", 5.49, 100, "Sharp"]
+condiment = ["Mustard", 1.99, 75, "Spicy"]
+# creating main list:
+deli_dept = [meat, cheese, condiment]
+print("Initial Deli List: <$deli_dept>", deli_dept)
+# identifying restock items:
+if "Ham" in meat and meat[2] < 100:
+    meat[2] = 100
+# Adding a seasonal meat item:
+seasonal_meat = ["Turkey", 4.50, 100, "Sliced"]
+# Updating the deli_dept list:
+deli_dept.append(seasonal_meat)
+deli_dept.remove(condiment)
+deli_dept.sort()
+print("Updated Deli List: <$deli_dept>", deli_dept)
